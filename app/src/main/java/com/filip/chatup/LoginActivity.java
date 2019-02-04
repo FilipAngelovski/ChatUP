9e0a08232713cd990da0ca6563521ba8135f283e
Filip Angelovski <f.angelovski88@gmail.com>
2019-02-04 17:32:44 +0100

    Adds activities
    Adds the complete set of activities for the application

--- app/src/main/java/com/filip/chatup/LoginActivity.java
+++ app/src/main/java/com/filip/chatup/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.filip.chatup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        final Intent intent = new Intent(this, SignUpActivity.class);
+        final Button button = findViewById(R.id.buttonSignUp);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
     }
+
 }
