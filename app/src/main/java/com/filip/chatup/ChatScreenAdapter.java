ca7515d097ef19e98a0c4d0c4866485850d632b4
Filip Angelovski <f.angelovski88@gmail.com>
2019-03-11 17:24:46 +0100

    Started implementation of recycler view for the chat screen
    Adds two layouts and an adapter class for the chat screen

---
+++ app/src/main/java/com/filip/chatup/ChatScreenAdapter.java
@@ -0,0 +1,15 @@
+package com.filip.chatup;
+
+import java.util.List;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+
+
+public class ChatScreenAdapter extends RecyclerView.Adapter<ChatScreenAdapter.ViewHolder> {
+}
